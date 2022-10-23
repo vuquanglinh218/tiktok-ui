68d67eb3392315bfd92ada986bc43820da6a2059
vuquanglinh218 <vuquanglinh218@gmail.com>
2022-10-23 09:48:15 +0700

    prettierrc

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import '~/App.css';
+import Button from '~/component/Button';
 
 function App() {
   return (
     <div className="App">
+      <Button></Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
---
+++ src/component/Button/index.js
@@ -0,0 +1,6 @@
+import Alert from '~/Alert';
+function Button() {
+  return <button onClick={Alert}>Alert</button>;
+}
+
+export default Button;
